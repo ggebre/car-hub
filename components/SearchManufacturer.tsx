41ebad957f8cb47cfbece7edcc33ac31351fdc69
Getu Gebre <getu.gebre.gg@gmail.com>
2023-09-05 22:08:56 -0500

    update the filter condtions for the search

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,8 +1,14 @@
 'use client';
+
+import { useState, Fragment} from "react";
 import { Combobox, Transition } from "@headlessui/react"
 import Image from 'next/image';
+import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types"
 function SearchManufacturer({manufacturer, setManufacturer} : SearchManufacturerProps) {
+  const [query, setQuery] = useState("");
+  // filter those manufactures which match the query 
+  const filteredManufacturers = query === "" ? manufacturers : manufacturers.filter((item) => item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, "")))
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -16,6 +22,22 @@ function SearchManufacturer({manufacturer, setManufacturer} : SearchManufacturer
             alt="Car logo"
             />
           </Combobox.Button>
+          <Combobox.Input         className="search-manufacturer__input"
+          placeholder="Volkswagen"
+          displayValue={(manufacturer: string) => manufacturer}
+          onChange={(e) => setQuery(e.target.value)}
+          />
+        <Transition 
+         as={Fragment}
+         leave="transition ease-in duration-100"
+         leaveFrom="opacity-100"
+         leaveTo="opacity-0"
+         afterLeave={() => setQuery('')}>
+          <Combobox.Options>
+
+          </Combobox.Options>
+
+         </Transition>
         </div>
       </Combobox>
     </div>
