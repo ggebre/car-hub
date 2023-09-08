6b41340b2c3f9e25d0e9020d84dd76210181b610
Getu Gebre <getu.gebre.gg@gmail.com>
2023-09-07 23:10:15 -0500

    update fetchCars util func

--- utils/index.ts
+++ utils/index.ts
@@ -1,32 +1,31 @@
 
 export async function fetchCars() {
-    // const headers = {
-    //     'X-RapidAPI-Key': `${process.env.X_RapidAPI_Key}`,
-    //     // 'X-Api-Key' : `${process.env.X_RapidAPI_Key}`,
-    //     'X-RapidAPI-Host': 'ccars-by-api-ninjas.p.rapidapi.com'
-    //   }
-    // const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars", {
-    //     method: 'GET',
-    //     headers: headers, 
-    // });
-    // const result = await response.json();
-    // console.log(result)
-    // return result;
+    const headers = {
+        'X-RapidAPI-Key': `${process.env.X_RapidAPI_Key}`,
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+      }
+    const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla", {
+        method: 'GET',
+        headers: headers, 
+    });
+    const result = await response.json();
+    
+    return result;
 
-    return [{
-        city_mpg: 20,
-        class: "luxury",
-        combination_mpg: 21,
-        cylinders: 6,
-        displacement: 10,
-        drive: "FWD",
-        fuel_type: "regular",
-        highway_mpg: 35,
-        make: "Toyota",
-        model: "camry",
-        transmission: "a",
-        year: 1990,
-    }]
+    // return [{
+    //     city_mpg: 20,
+    //     class: "luxury",
+    //     combination_mpg: 21,
+    //     cylinders: 6,
+    //     displacement: 10,
+    //     drive: "FWD",
+    //     fuel_type: "regular",
+    //     highway_mpg: 35,
+    //     make: "Toyota",
+    //     model: "camry",
+    //     transmission: "a",
+    //     year: 1990,
+    // }]
 
 }
 
