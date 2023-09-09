0fa0b744311ec0d360175e504f7bac3a88fafb56
Getu Gebre <getu.gebre.gg@gmail.com>
2023-09-08 22:51:11 -0500

    update fetchCars so that it accepts searchparameters

--- types/index.ts
+++ types/index.ts
@@ -32,8 +32,13 @@ export interface CarProps {
     model: string;
     transmission: string;
     year: number;
+}
 
-
-
+export interface FilterProps {
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
 }
 
